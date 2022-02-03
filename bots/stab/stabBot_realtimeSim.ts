6fcb87f8c3f18d8736773295657fcb0fb76b099a
superRaptor911 <raptor.inc2018@gmail.com>
2022-02-03 20:08:20 +0530

    realtimeSim

---
+++ bots/stab/stabBot_realtimeSim.ts
@@ -0,0 +1,130 @@
+import {changePercent, sleep} from '../../Utility';
+import {
+  api_getCoinPrices,
+  getBuyPrice,
+  getCurPrice,
+  getSellPrice,
+} from '../helper';
+import {COINS} from './stabConfig';
+
+const botConfig = {
+  iterations: 300,
+  waitTime: 300000,
+  buyFactor: 4,
+  sellFactor: 9,
+  amount: 300,
+};
+
+let wallet: any = {};
+
+interface transStruct {
+  asset: string;
+  price: number;
+  isValid: boolean;
+}
+
+function resetWallet(balance: number) {
+  wallet = {
+    balance: balance,
+  };
+
+  COINS.forEach(item => (wallet[item] = 0));
+}
+
+function sell(price: number, coinId: string, amount: number) {
+  if (wallet[coinId] >= amount) {
+    const total = amount * price;
+    wallet.balance += total * 0.998;
+    wallet[coinId] -= amount;
+    return true;
+  }
+  return false;
+}
+
+function buy(price: number, coinId: string, amount: number) {
+  amount = Math.min(wallet.balance, amount);
+  if (amount > 50) {
+    const count = amount / price;
+    wallet[coinId] += count;
+    wallet.balance -= amount * 1.002;
+    return true;
+  }
+  return false;
+}
+
+function getNetWorth(prices: any) {
+  COINS.forEach(item => {
+    sell(prices[item], item, wallet[item]);
+  });
+  const total = wallet.balance;
+  return Number(total.toFixed(2));
+}
+
+function logic(
+  trans: transStruct[],
+  asset: string,
+  price: number,
+  change: number,
+  buyPrice: number,
+  sellPrice: number,
+) {
+  let assetAv = true;
+  for (const i of trans) {
+    if (i.asset == asset) {
+      assetAv = false;
+      const changeSince = changePercent(i.price, price);
+      // stop loss
+      if (changeSince < -botConfig.sellFactor * 0.75) {
+        console.log(`Selling stop loss (${i.price},${price})`);
+        sell(sellPrice, asset, wallet[asset]);
+        i.isValid = false;
+      }
+      if (changeSince > botConfig.sellFactor) {
+        console.log(`Selling profit (${i.price},${price})`);
+        sell(sellPrice, asset, wallet[asset]);
+        i.isValid = false;
+      }
+    }
+  }
+
+  if (assetAv && change > botConfig.buyFactor) {
+    console.log(`--Buying ${asset}--`);
+    const result = buy(buyPrice, asset, 300);
+    trans.push({asset: asset, price: price, isValid: result});
+  }
+}
+
+async function mainFunc() {
+  resetWallet(1000);
+  let i = 0;
+  let previousPrices: any = {};
+  COINS.forEach(item => (previousPrices[item] = 0));
+  let trans: transStruct[] = [];
+
+  while (i < botConfig.iterations) {
+    const prices = await api_getCoinPrices();
+    for (let coinId of COINS) {
+      const price = getCurPrice(prices, coinId);
+      if (previousPrices[coinId] != 0) {
+        const change = changePercent(previousPrices[coinId], price);
+        console.log(`5 Min change for ${coinId} is ${change.toFixed(2)}`);
+        logic(
+          trans,
+          coinId,
+          price,
+          change,
+          getBuyPrice(prices, coinId),
+          getSellPrice(prices, coinId),
+        );
+      }
+      trans = trans.filter(item => item.isValid);
+      previousPrices[coinId] = price;
+    }
+    await sleep(botConfig.waitTime);
+    console.log('----END--OF--Iteration--', i, '----');
+    i++;
+  }
+  console.log('End : net worth ', getNetWorth(previousPrices));
+}
+
+mainFunc();
